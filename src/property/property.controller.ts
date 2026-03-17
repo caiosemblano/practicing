a6977c02d6bd432e3528b7b42e48ca89cad9f731
caiosemblano <semblanocaio@gmail.com>
2026-03-17 13:54:43 -0300

    Update controllers and specs

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('property')
-export class PropertyController {}
+export class PropertyController {
+
+    @Get()
+    findAll() {
+        return 'This action returns all properties';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} property`;
+    }
+
+    @Post()
+    create(@Body() createPropertyDto: CreatePropertyDto) {
+        return 'This action adds a new property';
+    }
+
+    
+}
