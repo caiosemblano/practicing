a00471d299455566562ad67d144f2a5fafb47ebd
caiosemblano <semblanocaio@gmail.com>
2026-03-17 17:30:47 -0300

    .

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { CreatePropertyDto } from './dto/create-property.dto';
+import { UpdatePropertyDto } from './dto/update-property.dto';
 
 @Controller('property')
 export class PropertyController {
@@ -16,6 +18,16 @@ export class PropertyController {
     @Post()
     create(@Body() createPropertyDto: CreatePropertyDto) {
         return 'This action adds a new property';
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updatePropertyDto: UpdatePropertyDto) {
+        return `This action updates a #${id} property`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} property`;
     }
 
     
